d166836899dacc086a2147398f2063e4b98cd291
airton-sudo <73069455+airton-sudo@users.noreply.github.com>
2023-11-22 00:52:24 +0000

    fix cabeçalho

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,19 +1,19 @@
 export default function Header() {
     return (
         <header className="text-center py-8 border-b border-[#4d4d4d] md:flex itens-center justify-between">
-            <h4 style={{ color: "black", fontSize: "2.1em", fontFamily: "Arial", paddingLeft: "20px" }}>escolahightech</h4>
+            <h4 style={{ color: "black", fontSize: "2em", fontFamily: "Arial", paddingLeft: "20px" }}>escolahightech</h4>
             <nav>
                 <a className="hover:text-[#00e77f]" href="#" style={{ marginRight: "100px" }}>
-                    Home  
+                    Home  
                 </a>
                 <a className="hover:text-[#00e77f]" href="#sobre-nos" style={{ marginRight: "100px" }}>
-                    Sobre Nós  
+                    Sobre Nós  
                 </a>
                 <a className="hover:text-[#00e77f]" href="#contato" style={{ marginRight: "100px" }}>
-                    Contato  {" "}
+                    Contato  {" "}
                 </a>
                 <a className="hover:text-[#00e77f]" href="#" style={{ marginRight: "100px" }}>
-                    Login  
+                    Login  
                 </a>
             </nav>
         </header>
